8f5a42620b1fc584d5a1eeedd6cc99f981db3ba5
Byan Aditya <aditya.byan00@gmail.com>
2025-07-30 21:21:45 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -327,3 +327,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
     }
+
+// === aplikasi===
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js')
+          .then(reg => console.log('✅ Service Worker registered!', reg))
+          .catch(err => console.log('❌ Service Worker registration failed:', err));
+      });
+    }
